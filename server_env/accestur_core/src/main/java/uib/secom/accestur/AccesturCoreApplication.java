1dc6cf769ea28db55aae46e973de38293c1f4bd0
uib <Pau Conejero>
2017-03-10 16:25:54 +0100

    Add main skeleton for the project

---
+++ server_env/accestur_core/src/main/java/uib/secom/accestur/AccesturCoreApplication.java
@@ -0,0 +1,12 @@
+package uib.secom.accestur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesturCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesturCoreApplication.class, args);
+	}
+}
---
+++ server_env/accestur_front/src/main/java/uib/secom/accestur/AccesturFrontApplication.java
@@ -0,0 +1,12 @@
+package uib.secom.accestur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesturFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesturFrontApplication.class, args);
+	}
+}
