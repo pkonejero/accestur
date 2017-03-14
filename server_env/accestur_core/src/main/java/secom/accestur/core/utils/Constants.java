6db00fc33c5e721ecfc89a8a9a09f31f3a96c4bd
uib <Pau Conejero>
2017-03-14 16:53:40 +0100

    Add file for utils - Constants

---
+++ server_env/accestur_core/src/main/java/secom/accestur/core/utils/Constants.java
@@ -0,0 +1,6 @@
+package secom.accestur.core.utils;
+
+public abstract class Constants{
+	public static final Integer ELGAMAL_DEFAULT_BITS = 256;
+	public static final Integer RSA_DEFAULT_BITS = 256;
+}
