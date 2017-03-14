22483cddd5768200701e453085822876414af86d
uib <Pau Conejero>
2017-03-14 19:53:53 +0100

    Add JPA + HSQLDB dependency for DB

--- server_env/src/main/java/Application.java
+++ server_env/src/main/java/Application.java
@@ -1,9 +1,11 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"secom.accestur.front","secom.accestur.core"})
+@EnableJpaRepositories
 public class Application {
 
 	public static void main(final String... args) {
