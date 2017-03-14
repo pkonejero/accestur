8f38238cebeeb0b72a78c8d704371a8aad278d3f
uib <Pau Conejero>
2017-03-14 14:54:13 +0100

    Add thymeleaf layer for UI/VIEW framework

--- server_env/accestur_front/src/main/java/secom/accestur/front/controllers/HomePageController.java
+++ server_env/accestur_front/src/main/java/secom/accestur/front/controllers/HomePageController.java
@@ -1,24 +1,20 @@
 package secom.accestur.front.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import secom.accestur.core.crypto.elgamal.*;
-import secom.accestur.core.service.impl.IssuerService;
-
-@RestController
+@Controller
 public class HomePageController{
-	
-	@Autowired
-	@Qualifier("issuerService")
-	private IssuerService issuerService;
-	
-	@RequestMapping("/**")
-	public String homepage(){
-		Elgamal eg=new Elgamal(256);
-		String s="Lorem ipsum dolor sit amet, consectetur adipisicing elit";
-		return "Framework - ACCESTUR - " + eg.Elgamal_PtToString(eg.decrypt(eg.encrypt(s)));
+
+	@Value("${welcome.message:test}")
+	private String message;
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model){
+		model.put("message", this.message);
+		return "welcome";
 	}
 }
