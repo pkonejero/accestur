42279c5e5d19478619c4b56c23c287701f15c491
uib <Pau Conejero>
2017-03-15 13:16:26 +0100

    Add configuration to scan enities for Hibernate

--- server_env/src/main/java/Application.java
+++ server_env/src/main/java/Application.java
@@ -1,11 +1,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"secom.accestur.front","secom.accestur.core"})
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = {"secom.accestur.core"})
+@EntityScan(basePackages = {"secom.accestur.core"})
 public class Application {
 
 	public static void main(final String... args) {
