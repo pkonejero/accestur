3cfff16d22dbdd70a8752a7a1245b2eb5f9ec635
uib <Pau Conejero>
2017-03-29 15:50:35 +0200

    Test Example - Save / Get Info from DB

--- server_env/accestur_core/src/main/java/secom/accestur/core/facade/UserFacadeInterface.java
+++ server_env/accestur_core/src/main/java/secom/accestur/core/facade/UserFacadeInterface.java
@@ -1,7 +1,10 @@
 package secom.accestur.core.facade;
 
+import secom.accestur.core.model.User;
 import secom.accestur.core.service.impl.UserService;
 
 public interface UserFacadeInterface extends FacadeInterface{	
 	public UserService getUserService();
+	
+	public User getUser();
 }
