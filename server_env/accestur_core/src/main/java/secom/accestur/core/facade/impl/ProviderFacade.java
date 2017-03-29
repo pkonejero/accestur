d160031c1f2667849c3631951e2469c1b2c7475b
tianet <sgalmesalba@gmail.com>
2017-03-29 16:11:53 +0200

    GetServices in facades

--- server_env/accestur_core/src/main/java/secom/accestur/core/facade/impl/ProviderFacade.java
+++ server_env/accestur_core/src/main/java/secom/accestur/core/facade/impl/ProviderFacade.java
@@ -1,8 +1,16 @@
 package secom.accestur.core.facade.impl;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import secom.accestur.core.facade.ProviderFacadeInterface;
+import secom.accestur.core.service.impl.ProviderService;
 
 @Component("providerFacade")
-public class ProviderFacade implements ProviderFacadeInterface{}
+public class ProviderFacade implements ProviderFacadeInterface{
+	@Autowired
+	@Qualifier("providerService")
+	private ProviderService providerService;
+	
+}
