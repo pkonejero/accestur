16fb5d7a4fd23af06cde2f60f530c73e88a375e1
gornals-PC\gornals <gornals2@gmail.com>
2017-04-13 13:17:14 +0200

    Added all interfaces for MCoupons

---
+++ server_env/accestur_core/src/main/java/secom/accestur/core/service/coupon/ActivationMCouponServiceInterface.java
@@ -0,0 +1,7 @@
+package secom.accestur.core.service.coupon;
+
+import secom.accestur.core.model.coupon.ActivationMCoupon;
+
+public interface ActivationMCouponServiceInterface{
+	public ActivationMCoupon getActivationByMCouponSn(long sn);
+}
