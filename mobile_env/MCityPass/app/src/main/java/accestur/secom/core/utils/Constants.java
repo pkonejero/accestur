6c6f77b13b9d7ee4f53fccb4baa1bdc79682352f
uib <Pau Conejero>
2017-04-18 19:36:49 +0200

    Prototype for Android APP - MCityPass

---
+++ mobile_env/MCityPass/app/src/main/java/accestur/secom/core/utils/Constants.java
@@ -0,0 +1,11 @@
+package accestur.secom.core.utils;
+
+public abstract class Constants{
+    public static final Integer PRIME_BITS = 512;
+    public static final Integer PRIME_CERTAINTY = 40;
+    public static final String LOREM = "Lorem ipsum dolor sit amet.";
+    public static final String LIFETIME = "" + 24*7*60*60*1000;
+    public static final String CATEGORY = "Adult";
+    public static final String EXPDATE = "08/06/2017";
+    public static final String TERMS_AND_CONDITIONS = "Lorem ipsum dolor sit amet.";
+}
