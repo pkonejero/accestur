c519a64a952677350e0a7e49d7d70c0d47da5068
tianet <sgalmesalba@gmail.com>
2017-04-24 11:26:51 +0200

    User in android

--- mobile_env/MCityPass/app/src/main/java/accestur/secom/core/crypto/Schnorr.java
+++ mobile_env/MCityPass/app/src/main/java/accestur/secom/core/crypto/Schnorr.java
@@ -3,6 +3,7 @@ package accestur.secom.core.crypto;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Random;
+
 import accestur.secom.core.utils.Constants;
 
 import org.json.*;
@@ -27,6 +28,11 @@ public class Schnorr {
 
     private BigInteger w1;
     private BigInteger w2;
+
+    private BigInteger a3;
+    private BigInteger A_3;
+    private BigInteger w3;
+    private JSONObject jsonObject;
 
     public Schnorr() {
     }
@@ -120,6 +126,36 @@ public class Schnorr {
 
         return first && second;
     }
+
+
+    public BigInteger sendRequest() {
+        a3 = new BigInteger(Constants.PRIME_BITS, new Random());
+        System.out.println(c);
+        A_3 = g.modPow(a3, p);
+        return A_3;
+    }
+
+    public BigInteger sendChallenge() {
+        c = new BigInteger(Constants.PRIME_BITS, new Random());
+        return c;
+    }
+
+    public BigInteger answerChallenge(BigInteger RU) {
+        BigInteger cRU = c.multiply(RU);
+        BigInteger a3_cRU = a3.add(cRU);
+        w3 = a3_cRU.mod(q);
+        return w3;
+    }
+
+    public boolean verifyChallenge(BigInteger Hu_C) {
+        BigInteger g_w3 = g.modPow(w3, p);
+
+        BigInteger hca = Hu_C.multiply(A_3);
+        BigInteger hcaMODp = hca.mod(p);
+
+        return g_w3.equals(hcaMODp);
+    }
+
 
     public BigInteger send_a_to_b_request() {
         c = new BigInteger(Constants.PRIME_BITS, new Random());
@@ -293,18 +329,43 @@ public class Schnorr {
         this.w2 = w2;
     }
 
+
+    public BigInteger getA3() {
+        return a3;
+    }
+
+    public void setA3(BigInteger a3) {
+        this.a3 = a3;
+    }
+
+    public BigInteger getA_3() {
+        return A_3;
+    }
+
+    public void setA_3(BigInteger a_3) {
+        A_3 = a_3;
+    }
+
+    public BigInteger getW3() {
+        return w3;
+    }
+
+    public void setW3(BigInteger w3) {
+        this.w3 = w3;
+    }
+
     public String getCertificate() {
-        JSONObject jsonObject = new JSONObject();
+        JSONObject jsonObject = null;
         try {
+            jsonObject = new JSONObject();
+
             jsonObject.put("p", p.toString());
             jsonObject.put("q", q.toString());
             jsonObject.put("g", g.toString());
             jsonObject.put("y", y.toString());
-        } catch (JSONException e1) {
-            e1.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-
         return jsonObject.toString();
     }
 
@@ -316,42 +377,40 @@ public class Schnorr {
             jsonObject.put("g", g.toString());
             jsonObject.put("y", y.toString());
             jsonObject.put("x", x.toString());
-        } catch (JSONException e1) {
-            e1.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-
         return jsonObject.toString();
     }
 
     public static Schnorr fromCertificate(String json) {
+        JSONObject jsonObject;
         Schnorr schnorr = new Schnorr();
         try {
-            JSONObject jsonObject = new JSONObject(json);
-            schnorr.setY((new BigInteger(jsonObject.getString("y"))));
+            jsonObject = new JSONObject(json);
             schnorr.setP((new BigInteger(jsonObject.getString("p"))));
             schnorr.setG((new BigInteger(jsonObject.getString("g"))));
             schnorr.setQ((new BigInteger(jsonObject.getString("q"))));
-        } catch (JSONException e1) {
-            e1.printStackTrace();
+            schnorr.setY((new BigInteger(jsonObject.getString("y"))));
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
         return schnorr;
     }
 
     public static Schnorr fromPrivateCertificate(String json) {
+        JSONObject jsonObject;
         Schnorr schnorr = new Schnorr();
         try {
-            JSONObject jsonObject = new JSONObject(json);
+            jsonObject = new JSONObject();
             schnorr.setP((new BigInteger(jsonObject.getString("p"))));
             schnorr.setG((new BigInteger(jsonObject.getString("g"))));
             schnorr.setQ((new BigInteger(jsonObject.getString("q"))));
             schnorr.setY((new BigInteger(jsonObject.getString("y"))));
             schnorr.setX((new BigInteger(jsonObject.getString("x"))));
-        } catch (JSONException e1) {
-            e1.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
         return schnorr;
     }
 
